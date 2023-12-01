14b6b95d986832081fb5cb9025f6235c10f030e3
Your Name <you@example.com>
2023-12-01 01:35:21 -0500

    adding changes on button task for noise rebote

--- code/P2/RGBHttp/main/ButtonTask.c
+++ code/P2/RGBHttp/main/ButtonTask.c
@@ -7,6 +7,8 @@
 // Variables para almacenar las pulsaciones de los botones
 extern volatile int button_1_press_count = 0;
 extern volatile int button_2_press_count = 0;
+static bool last_button_1_state = false;
+static bool last_button_2_state = false;
 
 #define DEBOUNCE_TIME_MS 50 // tiempo de debounce en milisegundos
 
@@ -16,22 +18,28 @@ static uint64_t last_button_2_press_time = 0;
 
 static void IRAM_ATTR button_1_isr_handler(void *arg)
 {
-    uint64_t now = esp_timer_get_time() / 1000; // Tiempo actual en milisegundos
-    if (now - last_button_1_press_time > DEBOUNCE_TIME_MS)
+    uint64_t now = esp_timer_get_time() / 1000;              // Tiempo actual en milisegundos
+    bool current_state = gpio_get_level(BUTTON_1_GPIO) == 0; // Estado actual del botón
+
+    if (now - last_button_1_press_time > DEBOUNCE_TIME_MS && !last_button_1_state && current_state)
     {
         button_1_press_count++;
         last_button_1_press_time = now;
     }
+    last_button_1_state = current_state;
 }
 
 static void IRAM_ATTR button_2_isr_handler(void *arg)
 {
-    uint64_t now = esp_timer_get_time() / 1000; // Tiempo actual en milisegundos
-    if (now - last_button_2_press_time > DEBOUNCE_TIME_MS)
+    uint64_t now = esp_timer_get_time() / 1000;              // Tiempo actual en milisegundos
+    bool current_state = gpio_get_level(BUTTON_2_GPIO) == 0; // Estado actual del botón
+
+    if (now - last_button_2_press_time > DEBOUNCE_TIME_MS && !last_button_2_state && current_state)
     {
         button_2_press_count++;
         last_button_2_press_time = now;
     }
+    last_button_2_state = current_state;
 }
 
 // Función para inicializar la tarea de botones
@@ -39,7 +47,7 @@ void button_task_initialize(void)
 {
     // Configurar los pines GPIO como entradas con pull-up
     gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_NEGEDGE,
+        .intr_type = GPIO_INTR_ANYEDGE,
         .pin_bit_mask = (1ULL << BUTTON_1_GPIO) | (1ULL << BUTTON_2_GPIO),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = 1,
